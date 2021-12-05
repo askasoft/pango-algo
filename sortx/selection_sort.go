bd8d531e0a4c877516651a92f420ad4af22eaa9e
Frank Wang <yf.frank.wang@outlook.com>
2021-12-05 20:47:10 +0800

    sortx: selection sort

---
+++ sortx/selection_sort.go
@@ -0,0 +1,22 @@
+package sortx
+
+// SelectionSort selection sort implementation
+// Time:  O(n^2)
+// Space: O(1)
+func SelectionSort(data Interface) {
+	n := data.Len()
+
+	m := 0 // minimum
+
+	for i := 0; i < n; i++ {
+		m = i
+		for j := i + 1; j < n; j++ {
+			if data.Less(j, m) {
+				m = j
+			}
+		}
+		if m != i {
+			data.Swap(m, i)
+		}
+	}
+}
---
+++ sortx/selection_sort_test.go
@@ -0,0 +1,12 @@
+package sortx
+
+import (
+	"testing"
+)
+
+func TestSelectionSort(t *testing.T) {
+	f := func(data Interface) {
+		SelectionSort(data)
+	}
+	testSortFunc(t, "SelectionSort", f)
+}
